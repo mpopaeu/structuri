cca0c39d2b297d76654e1ff6d1b477f2a1b51cf1
mpopaeu <emi.popa@gmail.com>
2020-04-01 11:30:10 +0300

    add new examples for 1054, week #6

--- 2019-2020/seminar/1054Sol/1054Proj/07_ListaSimpla.cpp
+++ 2019-2020/seminar/1054Sol/1054Proj/07_ListaSimpla.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <string.h>
+#include <stdlib.h>
 
-#define MY_STRING "Anonymous"
-#define ID_STUDENT 0
-#define NR_GRUPA "0000"
+#define LINESIZE 128
+
 
 struct Student {
 	int id;
@@ -21,7 +21,10 @@ struct Nod {
 Nod* inserareSfarsitLista(Nod* p, Student s) {
 	Nod* nou;
 	nou = (Nod*)malloc(sizeof(Nod)); // alocare nod nou
-	nou->stud = s; // asumare s.nume va fi copiat peste nou->nume
+	// nou->stud = s; // copiere inclusiv nrGrupa !!!
+	nou->stud.id = s.id;
+	nou->stud.nume = s.nume; // asumare s.nume va fi copiat peste nou->nume
+	strcpy(nou->stud.nrGrupa, s.nrGrupa);
 	nou->next = NULL; 
 
 	if (p == NULL) // lista nu contine nici un nod/element
@@ -45,23 +48,55 @@ Nod* inserareSfarsitLista(Nod* p, Student s) {
 // dezalocare lista
 
 // interschimb noduri adiacente lista simpla (cu modificarea adrese de legatura -> next)
+Nod* interschimbAdiacente(Nod* lst, int idStud) {
 
+	// cazuri particulare
+	//		1. lista contine cel mult 1 nod - se abandoneaza operatia
+	//		2. primul nod din interschimb este lst (se interschimba nodul 1 cu 2) - are loc interschimb, se returneaza noul inceput de lista
+	//		3. primul nod din interschimb este ultimul din lista (nu exista nodul 2) - se abandoneaza operatia
+
+	// caz general
+
+	return lst;
+}
 
 void main() {
 	Nod* prim = NULL;
 	Student s;
 
-	while (fgets(.....)) {
-		// alocare (nume) si initializare student
+	FILE * f;
+	f = fopen("Studenti.txt", "r");
 
+	char * token, file_buf[LINESIZE], sep_list[] = ",";
+	while (fgets(file_buf, sizeof(file_buf), f)) {
+		// alocare (nume) si initializare student
+		token = strtok(file_buf, sep_list);
+		s.id = atoi(token); // conversie ASCII->Binar(int)
+
+		token = strtok(NULL, sep_list);
+		s.nume = (char*)malloc((strlen(token) + 1) * sizeof(char));
+		strcpy(s.nume, token);
+
+		token = strtok(NULL, sep_list);
+		strcpy(s.nrGrupa, token);
+
+		token = strtok(NULL, sep_list); // verificare existenta token dupa medie in linia curenta file_buf
+		if (token)
+			printf("\nEroare preluare token!");
 		// apel functie inserare nod in lista simpla
 		prim = inserareSfarsitLista(prim, s);
+
+		s.nume = NULL;
 	}
 
 	// parsare lista simpla pentru validare inserari
 	Nod* temp = prim;
 	while (temp != NULL) {
-		printf(" Student id: %d\n", temp->stud.id);
+		printf(" Student id: %d, %s, %s\n", temp->stud.id, temp->stud.nume, temp->stud.nrGrupa);
+
+		temp = temp->next;
 	}
+
+	fclose(f);
 
 }
