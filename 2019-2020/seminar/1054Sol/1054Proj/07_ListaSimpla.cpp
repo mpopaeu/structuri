8d1c62a96d618580ede59adc98c27a25abfe8ff6
mpopaeu <emi.popa@gmail.com>
2020-04-01 11:51:44 +0300

    add new examples for 1054, week #6

--- 2019-2020/seminar/1054Sol/1054Proj/07_ListaSimpla.cpp
+++ 2019-2020/seminar/1054Sol/1054Proj/07_ListaSimpla.cpp
@@ -42,23 +42,60 @@ Nod* inserareSfarsitLista(Nod* p, Student s) {
 }
 
 // stergere nod din lista
+Nod* stergereNodInceput(Nod* lst) {
+	Nod * temp = lst;
+
+	if (lst) {
+		lst = lst->next; // actualizare nod de inceput lista
+
+		free(temp->stud.nume); // dezalocare mem heap pt nume student
+		free(temp);			// dezalocare mem heap pt nod din lista
+	}
+
+	return lst;
+}
 
 // deteminare numar noduri in lista
+int nrNoduri(Nod* lst) {
+	int n = 0;
 
-// dezalocare lista
+	Nod * temp = lst;
+	while (temp) {
+		n += 1; // exista nod curent, nr curent de noduri este incrementat
+
+		temp = temp->next; // temp este mutat pe nod succesor
+	}
+
+	return n;
+}
 
 // interschimb noduri adiacente lista simpla (cu modificarea adrese de legatura -> next)
 Nod* interschimbAdiacente(Nod* lst, int idStud) {
+	Nod *p, *q, *r, *s;
 
 	// cazuri particulare
 	//		1. lista contine cel mult 1 nod - se abandoneaza operatia
+	if (lst == NULL || lst->next == NULL)
+		return lst;
+	
 	//		2. primul nod din interschimb este lst (se interschimba nodul 1 cu 2) - are loc interschimb, se returneaza noul inceput de lista
+	if (lst->stud.id == idStud) {
+		// initializam cele 3 noduri implicate in interschimb
+		q = lst;
+		r = lst->next;
+		s = r->next;
+	}
+
 	//		3. primul nod din interschimb este ultimul din lista (nu exista nodul 2) - se abandoneaza operatia
 
 	// caz general
 
 	return lst;
 }
+
+// interschimb noduri oarecare in lista simpla (cu modificare de adrese de legatura)
+
+// sortarea unei liste simple (cu apel functie/functii de interschimb noduri dezvoltate/implementate mai sus)
 
 void main() {
 	Nod* prim = NULL;
@@ -90,7 +127,22 @@ void main() {
 	}
 
 	// parsare lista simpla pentru validare inserari
+	printf("Traversare lista simpla dupa creare:\n");
 	Nod* temp = prim;
+	while (temp != NULL) {
+		printf(" Student id: %d, %s, %s", temp->stud.id, temp->stud.nume, temp->stud.nrGrupa); // \n dupa fiecare nr grupa deoarece fgets pune LF (0x0a) in ultimul token de pe fiecare linie
+
+		temp = temp->next;
+	}
+
+	printf("\nNumarul de noduri din lista simpla dupa creare: %d\n", nrNoduri(prim));
+
+	// dezalocare lista simpla
+	while (prim)
+		prim = stergereNodInceput(prim);
+
+	printf("\nTraversare lista simpla dupa dezalocare:\n");
+	temp = prim;
 	while (temp != NULL) {
 		printf(" Student id: %d, %s, %s\n", temp->stud.id, temp->stud.nume, temp->stud.nrGrupa);
 
