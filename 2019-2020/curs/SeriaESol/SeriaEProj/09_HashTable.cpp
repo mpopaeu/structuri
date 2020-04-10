23d18be82d020a0cc361310e9286844944ddae06
mpopaeu <emi.popa@gmail.com>
2020-04-10 15:13:24 +0300

    add new examples for Seria E, week #8

--- 2019-2020/curs/SeriaESol/SeriaEProj/09_HashTable.cpp
+++ 2019-2020/curs/SeriaESol/SeriaEProj/09_HashTable.cpp
@@ -25,45 +25,120 @@ int positionHashFunction(char *str, int size) {
 
 // returns insertion success / failure
 char insertStudent(Student* ht, int size, Student s) {
-	// calculate the position to be used by storing the student
-	int pos = positionHashFunction(s.name, size);
-	printf("Position: %d for %s\n", pos, s.name); // cheie de cautare: nume student; afisare de control cu privire la rezultatul functiei hash
+	if (ht) {
+		// calculate the position to be used by storing the student
+		int pos = positionHashFunction(s.name, size);
+		printf("Position: %d for %s\n", pos, s.name); // cheie de cautare: nume student; afisare de control cu privire la rezultatul functiei hash
 
-	// search the first available starting with pos
-	char inserted = 0; // flag for insertion 
+		// search the first available starting with pos
+		char inserted = 0; // flag for insertion 
 
-					   // look for first available position in hash table
-					   // starting with pos and till size
-	for (int i = pos; i<size && !inserted; i++) {
-		if (ht[i].name == NULL) {
-			ht[i] = s;
-			inserted = 1;
+						   // look for first available position in hash table
+						   // starting with pos and till size
+		for (int i = pos; i < size && !inserted; i++) {
+			if (ht[i].name == NULL) {
+				ht[i] = s;
+				inserted = 1;
+			}
 		}
+
+		return inserted;
 	}
 
-	return inserted;
+	return 0;
 }
 
 // search a student using its name
 int searchStudent(Student* ht, int size, char* studName)
 {
-	int pos = positionHashFunction(studName, size);
-	char found = 0;
-	for (int i = pos; i<size && !found; i++)
-	{
-		if (ht[i].name == 0)
-			found = -1; // end of the cluster
-		else if (strcmp(ht[i].name, studName) == 0)
+	if (ht != NULL) {
+		int pos = positionHashFunction(studName, size);
+		char found = 0;
+		for (int i = pos; i < size && !found; i++)
 		{
-			found = 1;
-			pos = i;
+			if (ht[i].name == 0)
+				found = -1; // end of the cluster
+			else if (strcmp(ht[i].name, studName) == 0)
+			{
+				found = 1;
+				pos = i;
+			}
 		}
+
+		if (found == 1)
+			return pos; // student found within the hash table
+		else
+			return -1; // student not found
 	}
 
-	if (found == 1)
-		return pos; // student found within the hash table
-	else
-		return -1; // student not found
+	return -2; // tabela nu exista/ nu este alocata in mem heap
+}
+
+// delete a student in hash table using linear probing
+char deleteStudent(Student* ht, int size, char* studName)
+{
+	if (ht) {
+		// cauta pozitia finala a studentului in tabela hash
+		int poz = searchStudent(ht, size, studName);
+		if (poz == -1 || poz == -2)
+			return 0; // nu exista studentul cautat in tabela de dispersie
+					  // stergerea nu are loc
+
+		// "stergere" student
+		free(ht[poz].name);
+		ht[poz].name = 0; // pozitia se disponibilizeaza la inserare; stergere logica a studentului 
+		int inf, sup; // capete ale cluster-ului in care se sterge
+
+		// determinare limita inferioara cluster; sub-cluster stanga [inf; poz - 1]
+		char flag = 0;
+		for (int i = poz - 1; i >= 0 && !flag; i--)
+		{
+			if (ht[i].name == 0)
+			{
+				flag = 1;
+				inf = i + 1;
+			}
+		}
+		if (!flag)
+			inf = 0;
+
+		// determinare limita superioara cluster; sub-cluster dreapta [poz + 1; sup]
+		flag = 0;
+		for (int i = poz + 1; i < size && !flag; i++)
+			if (ht[i].name == 0)
+			{
+				flag = 1;
+				sup = i - 1;
+			}
+		if (!flag)
+			sup = size - 1;
+
+		// alocare vector temporar salvare studenti din cele 2 subclustere
+		// formate de stergere student
+		Student* temp = (Student*)malloc(sizeof(Student)*(sup - inf));
+		int  j = 0;
+		for (int i = inf; i < poz; i++)
+		{
+			temp[j++] = ht[i]; // mutare element din cluster in vector temporar
+			ht[i].name = 0; // disponibilizare pozitie in tabela hash
+		}
+		for (int i = poz + 1; i <= sup; i++)
+		{
+			temp[j++] = ht[i]; // mutare element din cluster in vector temporar
+			ht[i].name = 0; // disponibilizare pozitie in tabela hash
+		}
+
+		// reinserare studenti in tabela de dispersie
+		// foarte posibil ordine diferita in functie de punctele de coliziune
+		for (int i = 0; i < (sup - inf); i++)
+			flag = insertStudent(ht, size, temp[i]);
+		// dezalocarevector temporar
+		free(temp);
+
+		return 1; // stergere efectuata
+	}
+
+	return -1;
 }
 
 void main() {
@@ -149,11 +224,48 @@ void main() {
 		}
 	}
 
+	char numeStudent[] = "Popescu Gigel";
+
+	char sters = deleteStudent(HTable, size, numeStudent);
+
+	printf("\n Hash table content (size %d):\n", size);
+	for (unsigned int i = 0; i < size; i++) {
+		if (HTable[i].name) {
+			printf("Position: %d, Student: %s\n", i, HTable[i].name);
+		}
+	}
+
 	// cautare student dupa nume
-	char numeStudent[] = "Popa Marius";
 	int pozTabela = searchStudent(HTable, size, numeStudent);
-	if (pozTabela != -1)
+	if (pozTabela >= 0)
 		printf("Student identificat pe pozitia %d in tabela: %d %s\n\n", pozTabela, HTable[pozTabela].id, HTable[pozTabela].name);
 	else
-		printf("Studentul nu este in tabela de dispersie.\n\n");
+		printf("Studentul %s nu este in tabela de dispersie.\n\n", numeStudent);
+
+	// dezalocare tabela de dispersie
+	for (unsigned int i = 0; i < size; i++) {
+		if (HTable[i].name) {
+			free(HTable[i].name);
+		}
+	}
+	free(HTable); // dezalocare vector suport pentru tabela de dispersie
+	HTable = NULL;
+
+	pozTabela = searchStudent(HTable, size, numeStudent);
+
+	printf("\nTabela de dispersie dupa dezalocare:\n");
+	switch (pozTabela) {
+	case -1:
+		printf("Studentul %s nu este in tabela de dispersie.\n", numeStudent);
+		break;
+	case -2:
+		printf("Tabela de dispersie nu este alocata.\n", numeStudent);
+		break;
+	default:
+		printf("Student identificat pe pozitia %d in tabela: %d %s\n", pozTabela, HTable[pozTabela].id, HTable[pozTabela].name);
+		break;
+	}
+	
+
+	fclose(f);
 }
