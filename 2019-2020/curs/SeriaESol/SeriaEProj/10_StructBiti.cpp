3b81a1f398a14983233bd5709aedb2ecfe3a8e61
mpopaeu <emi.popa@gmail.com>
2020-04-24 15:28:57 +0300

    add new examples for Seria E, week #9

---
+++ 2019-2020/curs/SeriaESol/SeriaEProj/10_StructBiti.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+struct Car {
+	unsigned far : 1;
+	unsigned inchis : 1;
+	int kmBord;
+	unsigned trMetru : 4;
+	unsigned : 4;
+	unsigned AC : 1;
+	unsigned : 0;
+	unsigned flag : 1;
+};
+
+
+void main(int argc, char *argv[]) {
+
+	Car myCar;
+
+	char *pCar = (char*)&myCar;
+
+	for (int i = 0; i<sizeof(Car); i++) {
+		pCar[i] = 0x00;
+	}
+
+	myCar.far = 1;
+	myCar.inchis = 1;
+	myCar.trMetru = 10;
+	myCar.flag = 1;
+	myCar.AC = 1;
+
+	printf("Size Car = %d\n", sizeof(Car));
+	printf("\nStare far: %d, Maxim turatii/minut: %d\n", myCar.far, myCar.trMetru);
+
+}
