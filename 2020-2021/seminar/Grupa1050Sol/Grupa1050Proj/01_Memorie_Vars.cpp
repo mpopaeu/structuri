f9b1067116d2528382766dc29ea0df40f5a7ea61
Marius Popa <marius.popa@oracle.com>
2021-03-02 12:08:03 +0200

    add new examples for 1050, week #1

---
+++ 2020-2021/seminar/Grupa1050Sol/Grupa1050Proj/01_Memorie_Vars.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <malloc.h>
+
+int main()
+{
+	char x = 103; // [char, int,] [float, double, long double]
+				  // echivalent cu char x = 0x67; char x = 'g';
+				  // se aloca la compilare sizeof(char) bytes (1 byte) --> var locala lui main()
+	char vx[10];	// vector de elemente de tip char; sunt alocate 10 elemente la compilare (10 * sizeof(char) bytes = 10 bytes)
+					// var locala lui main()
+	char *px = NULL; // variabila de tip pointer; var locala lui main(), se aloca la compilare (stack seg)
+					// se aloca sizeof(px) bytes = 4 bytes: px este FAR seg:off
+	
+	px = &x; // incarc adresa de stack seg a lui x
+	*px = x + 3; // modific locatia x in mod indirect
+
+	px = vx; // incarc adresa de stack seg a lui vx
+	for (char i = 0; i < sizeof(vx); i++)
+		px[i] = x + i; // px[i] <--> *(px + i)
+
+	// alocarea la run-time
+	char n = sizeof(vx) - 2;
+	px = (char*)malloc(n * sizeof(char));
+
+	for (char i = 0; i < n; i++)
+		px[i] = vx[i] + i;
+
+	// dezalocare mem heap
+	free(px);
+	px = NULL;
+
+
+	return 0;
+}
