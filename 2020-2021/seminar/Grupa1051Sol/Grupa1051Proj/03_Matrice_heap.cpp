5e769d1adf551f98f209bb0f75ae6ee0c0900d7f
Marius Popa <marius.popa@oracle.com>
2021-03-17 19:27:35 +0200

    add new examples for 1051, week #4

--- 2020-2021/seminar/Grupa1051Sol/Grupa1051Proj/03_Matrice_heap.cpp
+++ 2020-2021/seminar/Grupa1051Sol/Grupa1051Proj/03_Matrice_heap.cpp
@@ -1,16 +1,180 @@
 #include <malloc.h>
+#include <stdio.h>
 
 #define INSERARE 1
 #define STERGERE 2
 
+// implementare Dulgheru Mihai
 int** op_elem_matrice(int** pMat, char &m, char * &dim_linii, char op_linie, int val_elem, char op_tip)
 {
 	// pMat - adresa matrice alocate in heap
 	// m - nr de linii al matricei
 	// dim_linii - vector cu dimensiuni reale/concrete la runtime ale liniilor
-	// op_linie - linia pt care se aplica operatia op_tip
-	// val_elem - valoare element pentru care se aplica operatia op_tip
+	// op_linie - linia pt care se aplica operatia op_tip; 
+	//				INSERARE: op_linie < m + 1
+	//				STERGERE: op_linie < m
+	// val_elem - valoare element pentru care se aplica operatia op_tip INSERARE / STERGERE
 	// op_tip - tip operatie: INSERARE / STERGERE
+
+	if (--op_linie < 0)
+	{
+		printf("Nu s-a efectuat nicio modificare. Linie invalida.\n");
+		return pMat;
+	}
+	if (op_tip == INSERARE)
+	{
+		if (op_linie < m + 1)
+		{
+			if (op_linie == m)
+			{
+				m++;
+				char* temp = (char*)malloc(m * sizeof(char));
+				for (char i = 0; i < m - 1; i++)
+				{
+					temp[i] = dim_linii[i];
+				}
+				temp[m - 1] = 1;
+				free(dim_linii);
+				dim_linii = temp;
+				int** matriceNoua = (int**)malloc(m * sizeof(int*));
+				for (char i = 0; i < m; i++)
+				{
+					matriceNoua[i] = (int*)malloc(dim_linii[i] * sizeof(int));
+				}
+				for (char i = 0; i < m - 1; i++)
+				{
+					for (char j = 0; j < dim_linii[i]; j++)
+					{
+						matriceNoua[i][j] = pMat[i][j];
+					}
+				}
+				matriceNoua[m - 1][dim_linii[m - 1] - 1] = val_elem;
+				for (char i = 0; i < m - 1; i++) {
+					free(pMat[i]);
+					//pMat[i] = NULL;
+				}
+				free(pMat);
+				pMat = matriceNoua;
+			}
+			else
+			{
+				dim_linii[op_linie]++;
+				int** matriceNoua = (int**)malloc(m * sizeof(int*));
+				for (char i = 0; i < m; i++)
+				{
+					matriceNoua[i] = (int*)malloc(dim_linii[i] * sizeof(int));
+				}
+				for (char i = 0; i < m; i++)
+				{
+					for (char j = 0; j < dim_linii[i]; j++)
+					{
+						if (i == op_linie && j == dim_linii[i] - 1)
+						{
+							matriceNoua[i][j] = val_elem;
+						}
+						else
+						{
+							matriceNoua[i][j] = pMat[i][j];
+						}
+					}
+				}
+				for (char i = 0; i < m; i++) {
+					free(pMat[i]);
+					// pMat[i] = NULL;
+				}
+				free(pMat);
+				pMat = matriceNoua;
+			}
+		}
+		else
+		{
+			printf("Nu s-a efectuat nicio modificare. Linie invalida.\n");
+		}
+	}
+	if (op_tip == STERGERE)
+	{
+		if (op_linie < m)
+		{
+			if (pMat[op_linie][dim_linii[op_linie] - 1] == val_elem)
+			{
+				if (dim_linii[op_linie] - 1 == 0)
+				{
+					m--;
+					char* temp = (char*)malloc(m * sizeof(char));
+					char i = 0;
+					while (i < op_linie)
+					{
+						temp[i] = dim_linii[i];
+						i++;
+					}
+					i++;
+					while (i < m + 1)
+					{
+						temp[i - 1] = dim_linii[i];
+						i++;
+					}
+					free(dim_linii);
+					dim_linii = temp;
+					int** matriceNoua = (int**)malloc(m * sizeof(int*));
+					for (i = 0; i < m; i++)
+					{
+						matriceNoua[i] = (int*)malloc(dim_linii[i] * sizeof(int));
+					}
+					for (i = 0; i < op_linie; i++)
+					{
+						for (char j = 0; j < dim_linii[i]; j++)
+						{
+							matriceNoua[i][j] = pMat[i][j];
+						}
+					}
+					for (i = op_linie + 1; i < m + 1; i++)
+					{
+						for (char j = 0; j < dim_linii[i - 1]; j++)
+						{
+							matriceNoua[i - 1][j] = pMat[i][j];
+						}
+					}
+					for (i = 0; i < m; i++) {
+						free(pMat[i]);
+						// pMat[i] = NULL;
+					}
+					free(pMat);
+					pMat = matriceNoua;
+				}
+				else
+				{
+					dim_linii[op_linie]--;
+					int** matriceNoua = (int**)malloc(m * sizeof(int*));
+					for (char i = 0; i < m; i++)
+					{
+						matriceNoua[i] = (int*)malloc((dim_linii[i]) * sizeof(int));
+					}
+					for (char i = 0; i < m; i++)
+					{
+						for (char j = 0; j < dim_linii[i]; j++)
+						{
+							matriceNoua[i][j] = pMat[i][j];
+						}
+					}
+					for (char i = 0; i < m; i++) {
+						free(pMat[i]);
+						// pMat[i] = NULL;
+					}
+					free(pMat);
+					pMat = matriceNoua;
+				}
+			}
+			else
+			{
+				printf("Nu s-a efectuat nicio modificare. Elementul pe care doriti sa-l stergeti nu exista.\n");
+			}
+		}
+		else
+		{
+			printf("Nu s-a efectuat nicio modificare. Linie invalida.\n");
+		}
+	}
+	return pMat;
 }
 
 int main()
---
+++ 2020-2021/seminar/Grupa1051Sol/Grupa1051Proj/04_Liste_simple.cpp
@@ -0,0 +1,47 @@
+#include <string.h>
+#include <malloc.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LINESIZE 128
+
+struct Student {
+	int id;
+	char* nume;
+	char nrGrupa[5];
+};
+
+struct Nod {
+	Student stud;
+	Nod* next;
+};
+
+
+int main() {
+	Nod* prim = NULL; // lista simpla empty
+	Student s;
+	FILE* f;
+	f = fopen("Studenti.txt", "r");
+
+	char * token, sep_list[] = ",\n";
+	char file_buf[LINESIZE];
+	while (fgets(file_buf, sizeof(file_buf), f)) {
+		// preluare continut fisier studenti; se initializeaza temporarul s
+		token = strtok(file_buf, sep_list);
+		s.id = atoi(token); // conversie ASCII->Binar(int)
+
+		token = strtok(NULL, sep_list);
+		s.nume = (char*)malloc((strlen(token) + 1) * sizeof(char));
+		strcpy(s.nume, token);
+
+		token = strtok(NULL, sep_list);
+		strcpy(s.nrGrupa, token);
+
+		// operatie pe lista simpla (inserare in lista simpla)
+
+
+		s.nume = NULL; // eliminare partajare zona hep pt ultimul student inserat
+	}
+
+	return 0;
+}
