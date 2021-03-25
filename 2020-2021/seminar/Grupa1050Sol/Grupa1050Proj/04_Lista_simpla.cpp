249bec5730b5697cf8d1410bff0a1a0c71a5026d
Marius Popa <marius.popa@oracle.com>
2021-03-25 14:45:55 +0200

    update examples for 1050, week #5

--- 2020-2021/seminar/Grupa1050Sol/Grupa1050Proj/04_Lista_simpla.cpp
+++ 2020-2021/seminar/Grupa1050Sol/Grupa1050Proj/04_Lista_simpla.cpp
@@ -28,6 +28,36 @@ Nod* inserare_nod(Nod* lst, Student st)
 	return nou;
 }
 
+Nod* stergere_nod(Nod* lst, Student &st)
+{
+	if (lst)
+	{
+		// lista contine cel putin 1 nod
+
+		// salvare date student in parametrul st
+		st.id = lst->stud.id;
+		st.nume = lst->stud.nume;
+		strcpy(st.nrGrupa, lst->stud.nrGrupa);
+
+		Nod *t = lst;
+
+		lst = lst->next; // actualizare inceput de lista (adresa nod 2, daca exista)
+		free(t);		// dezalocare nod 
+	}
+	else
+	{
+		// lista contine 0 noduri
+		st.nume = NULL;
+	}
+
+	return lst;
+}
+
+// functie care creeaza un vector cu studenti (din lista simpla) care fac parte din aceeasi grupa
+// input: lista simpla, nr grupa selectie studenti
+// output: vector de studenti (id, nume student) in aceeasi grupa, dimensiune vector; 
+// studentii din vector nu partajeaza zone de heap cu nodurile din lista simpla
+
 
 int main() {
 	Nod* prim = NULL; // lista simpla empty
@@ -55,7 +85,25 @@ int main() {
 		s.nume = NULL; // eliminare partajare zona heap pt ultimul student inserat
 	}
 
+	printf("Lista simpla dupa creare:\n");
+	Nod* t = prim;
+	while (t)
+	{
+		printf(" %d %s\n", t->stud.id, t->stud.nume); // prelucrarea unui nod din lista simpla
 
+		t = t->next;
+	}
+
+
+	// dezalocare lista simpla
+	printf("\n\nDezalocare lista simpla:\n");
+	while (prim)
+	{
+		prim = stergere_nod(prim, s);
+		printf(" %d %s\n", s.id, s.nume);
+
+		free(s.nume);
+	}
 
 	return 0;
 }
