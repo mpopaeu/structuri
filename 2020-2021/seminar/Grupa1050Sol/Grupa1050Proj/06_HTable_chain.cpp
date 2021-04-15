7e7c35add8862ef300bd598a441157f038949e43
Marius Popa <marius.popa@oracle.com>
2021-04-15 14:42:46 +0300

    add new examples for 1050, week #8

--- 2020-2021/seminar/Grupa1050Sol/Grupa1050Proj/06_HTable_chain.cpp
+++ 2020-2021/seminar/Grupa1050Sol/Grupa1050Proj/06_HTable_chain.cpp
@@ -95,7 +95,51 @@ Student cautare_tabela_hash(Nod** hTab, char * nume_student, int size)
 }
 
 // stergerea studentilor dintr-o grupa specificata 
-// dezalocarea tabelei de dispersie
+
+// lista cu cele mai multe coliziuni in tabela de dispersie (cea mai lunga lista simpla)
+// in - tabela de dispersie
+// in - dimensiune tabela de dispersie
+// out - nr de ordine in vector
+// out - nr de noduri din lista (nr de studenti)
+
+// TEMA - generalizare implementare pentru cazul in care exista mai multe liste cu acelasi nr maxim de noduri/studenti
+void calcul_max_coliziuni(Nod** hTab, int size, int &idx_list, int &max_elem)
+{
+	idx_list = -1; // index invalid pentru accesarea unei liste din tabela de dispersie hTab
+	max_elem = 0; // lista cu maxim 0 noduri corespunde unei tabele hash empty
+
+	for (int i = 0; i < size; i++)
+	{
+		if (hTab[i]) // exista lista i in tabela hTab
+		{
+			int nr_stud = 0;
+			Nod* tmp = hTab[i];
+			while (tmp)
+			{
+				nr_stud += 1;
+				tmp = tmp->next;
+			}
+
+			if (nr_stud > max_elem)
+			{
+				max_elem = nr_stud;
+				idx_list = i;
+			}
+		}
+	}
+}
+
+// extragere studenti din tabela de dispesie pe baza numelui specificat (cheie de cautare). 
+// Extragerea are loc intr-o structura lista simpla.
+// in - hTab tabela de dispersie
+// in - nume_student numele studentului cautat (poate fi valid pt mai multe noduri)
+// in - size dimensiunea tabela de dispersie
+// out - return lista simpla cu studentii extrasi din tabela de dispersie
+
+Nod* extragere_studenti_nume(Nod** hTab, char* nume_student, int size)
+{
+
+}
 
 int main() {
 
@@ -150,6 +194,34 @@ int main() {
 	{
 		printf("Studentul nu a fost identificat in tabela de dispersie.\n\n");
 	}
+
+	// determinare lista simpla cu maximul de coliziuni in tabela de dispersie
+	int idx_lista, max_stud;
+	calcul_max_coliziuni(HTable, DIM, idx_lista, max_stud);
+	if (max_stud == 0)
+	{
+		printf("\n Tabela de dispersie empty.\n");
+	}
+	else
+	{
+		printf("\n\n Maximul de coliziuni este in lista %d cu nr de studenti %d.\n", idx_lista, max_stud);
+	}
+
+	// dezalocarea tabelei de dispersie
+	for (int i = 0; i < DIM; i++) // dezalocare liste
+		if (HTable[i] != NULL) {  // lista i contine cel putin un student
+			Nod* tmp = HTable[i];
+			while (tmp) {
+				HTable[i] = HTable[i]->next; // mutare inceput de liste pe nodul 2
+
+				free(tmp->st.nume); // dezalocare nume student din nodul 1
+				free(tmp); // dezalocare nod 1
+
+				tmp = HTable[i]; // noul inceput de lista care se va dezaloca pe iteratia urmatoare
+			}
+		}
+	free(HTable); // dezalocare vector suport pentru tabela de dispersie
+	HTable = NULL;
 
 	fclose(f);
 
