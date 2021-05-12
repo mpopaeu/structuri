70af5e895d1dc6ffc943eb156b809a509e35702d
Marius Popa <marius.popa@oracle.com>
2021-05-12 12:46:33 +0300

    update examples for 1052, week #11

--- 2020-2021/seminar/Grupa1052Sol/Grupa1052Proj/07_Struct_Hash.cpp
+++ 2020-2021/seminar/Grupa1052Sol/Grupa1052Proj/07_Struct_Hash.cpp
@@ -50,6 +50,92 @@ int* inserare_cheie_Heap(int* strHeap, int& nMax, int &capacitate, int cheie)
 
 // TEMA 
 // operatia de extragere/stergere din structura max-heap
+int stergere_cheie_heap(int* strHeap, int& strSize)
+{
+	int key;
+	int offs_key = 0; // offset nod curent pentru care se aplica op de filtrare top-down
+	int max = -1; // offset descendent cu cheia maxima (max(left_key, right_key); pentru offs_key
+	//preiau ultimul element din structura
+	int lastElement = strHeap[strSize - 1];
+	key = strHeap[0];
+	strHeap[0] = lastElement; //ultimul element devine primul
+
+	strSize -= 1; //numarul de chei scade cu 1
+
+	int Left = 2 * offs_key + 1;
+	int Right = 2 * offs_key + 2;
+
+	if (Left < strSize && Right < strSize) // exista ambii descendenti
+	{
+		if (strHeap[Left] > strHeap[Right])
+		{
+			max = Left;
+		}
+		else
+		{
+			max = Right;
+		}
+	}
+	else
+	{
+		if (Left < strSize)
+		{
+			max = Left;
+		}
+	}
+
+	while (strSize > 1 && max != -1 && strHeap[max] > strHeap[offs_key]) //nu se respecta relatia de ordine 
+	{
+		// strSize > 0 - exista cel putin 2 elemente in structura
+		// max != -1 - exista cel putin 1 descendent pentru nodul curent strHeap[offs_key]
+		// strHeap[max] > strHeap[offs_key] - nu se respecta relatia de ordine pentru un max-heap
+		//interschimbare key-parinte
+		int aux = strHeap[offs_key];
+		strHeap[offs_key] = strHeap[max];
+		strHeap[max] = aux;
+
+		// actualizare offset key in vector
+		offs_key = max;
+
+		//calcul offset pentru noul key
+		Left = 2 * offs_key + 1;
+		Right = 2 * offs_key + 2;
+		if (Left < strSize && Right < strSize)
+		{
+			if (strHeap[Left] > strHeap[Right])
+			{
+				max = Left;
+			}
+			else
+			{
+				max = Right;
+			}
+		}
+		else
+		{
+			if (Left < strSize)
+			{
+				max = Left;
+			}
+			else
+			{
+				max = -1;
+			}
+		}
+	}
+
+	return key;
+}
+
+// creare vector cu jumatate din nr efective de chei stocate de structura max-heap
+// functie
+// [in] strHeap - vector suport pentru structura max-heap
+// [in,out] strSize - nr efectiv de chei din structura max-heap
+
+int* creare_vector_chei(int* strHeap, int& strSize)
+{
+
+}
 
 int main()
 {
@@ -84,6 +170,18 @@ int main()
 		printf(" %d ", sHeap[i]);
 
 	printf("\n\n");
+
+	if (nMax > 0)
+	{
+		cheie = stergere_cheie_heap(sHeap, nMax);
+		printf("Cheie extrasa din max-heap: %d\n", cheie);
+
+		printf("\nStructura Heap dupa extragere/stergere: ");
+		for (int i = 0; i < nMax; i++)
+			printf(" %d ", sHeap[i]);
+
+		printf("\n\n");
+	}
 
 	// dezalocare vector
 	free(sHeap);
