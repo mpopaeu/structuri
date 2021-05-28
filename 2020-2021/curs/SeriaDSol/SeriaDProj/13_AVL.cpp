356fc2e0cb60b518e9b9edf2d6c85f20d25831fa
Marius Popa <marius.popa@oracle.com>
2021-05-28 12:25:53 +0300

    add new examples for Seria D, week #13

--- 2020-2021/curs/SeriaDSol/SeriaDProj/13_AVL.cpp
+++ 2020-2021/curs/SeriaDSol/SeriaDProj/13_AVL.cpp
@@ -1,6 +1,10 @@
 
 #include <stdio.h>
 #include <malloc.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define LINESIZE 128
 
 // Definire structuri 
 
@@ -48,6 +52,7 @@ NodAVL* rotireSimplaDr(NodAVL* pivot, NodAVL* fiuSt) {
 	fiuSt->dr = pivot;
 	calculGENod(fiuSt);
 
+	printf("\n Rotire simpla la dreapta!\n");
 	return fiuSt;
 }
 
@@ -58,6 +63,7 @@ NodAVL* rotireSimplaSt(NodAVL* pivot, NodAVL* fiuDr) {
 	fiuDr->st = pivot;
 	calculGENod(fiuDr);
 
+	printf("\n Rotire simpla la stanga!\n");
 	return fiuDr;
 }
 
@@ -71,6 +77,7 @@ NodAVL* rotireDblStDr(NodAVL* pivot, NodAVL* fiuSt) {
 	fiuSt = rotireSimplaDr(pivot, fiuSt);
 	calculGENod(fiuSt);
 
+	printf("\n Rotire dubla stanga-dreapta!\n");
 	return fiuSt;
 }
 
@@ -84,6 +91,7 @@ NodAVL* rotireDblDrSt(NodAVL* pivot, NodAVL* fiuDr) {
 	fiuDr = rotireSimplaSt(pivot, fiuDr);
 	calculGENod(fiuDr);
 
+	printf("\n Rotire dubla dreapta-dreapta!\n");
 	return fiuDr;
 }
 
@@ -140,8 +148,51 @@ NodAVL* insNodAVL(NodAVL* r, Student s, int* err) {
 void InordineAVL(NodAVL* rAVL) {
 	if (rAVL) {
 		InordineAVL(rAVL->st);
-		printf(" %d %d ", rAVL->stud.id, rAVL->GE);
+		printf(" stud.id = %d, GE = %d \n", rAVL->stud.id, rAVL->GE);
 		InordineAVL(rAVL->dr);
 	}
 }
 
+int main()
+{
+	Student stud;
+	NodAVL* root = NULL;
+
+	FILE* f;
+	f = fopen("Studenti.txt", "r");
+	char studBuff[20];
+
+	char* token, file_buf[LINESIZE], sep_list[] = ",\n";
+
+	while (fgets(file_buf, sizeof(file_buf), f)) {
+		token = strtok(file_buf, sep_list);
+		stud.id = atoi(token);
+
+		token = strtok(NULL, sep_list);
+		stud.nume = (char*)malloc((strlen(token) + 1) * sizeof(char));
+		strcpy(stud.nume, token);
+
+		token = strtok(NULL, sep_list);
+		stud.medie = atof(token);
+
+		token = strtok(NULL, sep_list);
+		if (token)
+			printf("\nEroare preluare token!");
+
+		// inserare student in ABC
+		int err = 0;
+		root = insNodAVL(root, stud, &err);
+		if (err) {
+			printf("\nStudentul cu id %d exista in arbore.\n", stud.id);
+			free(stud.nume);
+		}
+		else
+			printf("\nStudentul %s a fost inserat\n", stud.nume);
+	}
+
+	InordineAVL(root);
+
+	fclose(f);
+
+	return 0;
+}
