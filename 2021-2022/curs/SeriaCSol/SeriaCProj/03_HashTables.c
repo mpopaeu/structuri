0421e0a374c011d8d2aa7ffa62dcb9c97881f0f7
Marius Popa <marius.popa@oracle.com>
2022-05-06 18:04:40 +0300

    add examples, week #8

--- 2021-2022/curs/SeriaCSol/SeriaCProj/03_HashTables.c
+++ 2021-2022/curs/SeriaCSol/SeriaCProj/03_HashTables.c
@@ -22,7 +22,7 @@ int positionHashFunction(char* str, int size) {
 
 char insertStudent(struct Student* ht, int size, struct Student s) {
 	int pos = positionHashFunction(s.name, size);
-	printf("Position: %d for %s\n", pos, s.name);
+	// printf("Position: %d for %s\n", pos, s.name);
 
 	char inserted = 0;
 
@@ -34,6 +34,80 @@ char insertStudent(struct Student* ht, int size, struct Student s) {
 	}
 
 	return inserted;
+}
+
+int searchStudent(struct Student* ht, int size, char* studName)
+{
+	int pos = positionHashFunction(studName, size);
+	char found = 0;
+	for (int i = pos; i < size && !found; i++)
+	{
+		if (ht[i].name == NULL)
+			found = -1;
+		else if (strcmp(ht[i].name, studName) == 0)
+		{
+			found = 1;
+			pos = i;
+		}
+	}
+
+	if (found == 1)
+		return pos;
+	else
+		return -1; // studentul nu a fost identificat in tabela de dispersie
+}
+
+char deleteStudent(struct Student* ht, int size, char* studName)
+{
+	int poz = searchStudent(ht, size, studName);
+	if (poz == -1)
+		return 0;
+
+	free(ht[poz].name);
+	ht[poz].name = NULL;
+	int inf, sup;
+
+	char flag = 0;
+	for (int i = poz - 1; i >= 0 && !flag; i--)
+	{
+		if (ht[i].name == 0)
+		{
+			flag = 1;
+			inf = i + 1;
+		}
+	}
+	if (!flag)
+		inf = 0;
+
+	flag = 0;
+	for (int i = poz + 1; i < size && !flag; i++)
+		if (ht[i].name == 0)
+		{
+			flag = 1;
+			sup = i - 1;
+		}
+	if (!flag)
+		sup = size - 1;
+
+	struct Student* temp = (struct Student*)malloc(sizeof(struct Student) * (sup - inf));
+	int  j = 0;
+	for (int i = inf; i < poz; i++)
+	{ // salvare studenti in vectorul temp (subcluster stanga)
+		temp[j++] = ht[i];
+		ht[i].name = NULL;
+	}
+	for (int i = poz + 1; i <= sup; i++)
+	{ // salvare studenti in vectorul temp (subcluster dreapta)
+		temp[j++] = ht[i];
+		ht[i].name = NULL;
+	}
+
+	for (int i = 0; i < (sup - inf); i++)
+		flag = insertStudent(ht, size, temp[i]);
+
+	free(temp);
+
+	return 1;
 }
 
 void main() {
@@ -96,6 +170,45 @@ void main() {
 			}
 		}
 	}
+
+	// cautare student in tabela de dispersie
+	int pos_student = searchStudent(HTable, size, "Pavel Ionescu");
+	if (pos_student != -1)
+	{
+		// student identificat in tabela de dispersie
+		printf("\nStudent identificat: %d %s", HTable[pos_student].id, HTable[pos_student].name);
+	}
+	else
+		printf("\nStudentul cautat nu este in tabela de dispersie!");
+
+	printf("\nTabela de dispersie inainte de stergere student:\n");
+	for (unsigned int i = 0; i < size; i++)
+	{
+		if (HTable[i].name != NULL)
+		{
+			printf("\nPosition: %d %s", i, HTable[i].name);
+		}
+	}
+	char sters = deleteStudent(HTable, size, "Popescu Gigel");
+	if (sters)
+	{
+		printf("\n\nTabela de dispersie dupa stergere student:\n");
+		for (unsigned int i = 0; i < size; i++)
+		{
+			if (HTable[i].name != NULL)
+			{
+				printf("\nPosition: %d %s", i, HTable[i].name);
+			}
+		}
+	}
+	else
+		printf("\nStudentul nu a fost gasit in tabela de dispersie.\n");
+
+	// dezalocare tabela de dispersie
+	for (unsigned int i = 0; i < size; i++)
+		if (HTable[i].name != NULL)
+			free(HTable[i].name);
+	free(HTable);
 
 	fclose(f);
 }
--- 2021-2022/curs/SeriaDSol/SeriaDProj/03_TabeleHash.c
+++ 2021-2022/curs/SeriaDSol/SeriaDProj/03_TabeleHash.c
@@ -14,7 +14,7 @@ struct Student {
 
 int positionHashFunction(char* str, int size) {
 	int sum = 0;
-	for (int i = 0; i < strlen(str); i++)
+	for (unsigned int i = 0; i < strlen(str); i++)
 		sum += str[i];
 
 	return sum % size;
@@ -36,6 +36,80 @@ char insertStudent(struct Student* ht, int size, struct Student s) {
 	return inserted;
 }
 
+int searchStudent(struct Student* ht, int size, char* studName)
+{
+	int pos = positionHashFunction(studName, size);
+	char found = 0;
+	for (int i = pos; i < size && !found; i++)
+	{
+		if (ht[i].name == 0) // sfarsit de cluster de studenti in vectorul suport al tabelei hash
+			found = -1;
+		else if (strcmp(ht[i].name, studName) == 0)
+		{
+			found = 1;
+			pos = i;
+		}
+	}
+
+	if (found == 1)
+		return pos;
+	else
+		return -1;
+}
+
+char deleteStudent(struct Student* ht, int size, char* studName)
+{
+	int poz = searchStudent(ht, size, studName);
+	if (poz == -1)
+		return 0;
+
+	free(ht[poz].name);
+	ht[poz].name = NULL;
+	int inf, sup;
+
+	char flag = 0;
+	for (int i = poz - 1; i >= 0 && !flag; i--)
+	{
+		if (ht[i].name == NULL)
+		{
+			flag = 1;
+			inf = i + 1;
+		}
+	}
+	if (!flag)
+		inf = 0;
+
+	flag = 0;
+	for (int i = poz + 1; i < size && !flag; i++)
+		if (ht[i].name == NULL)
+		{
+			flag = 1;
+			sup = i - 1;
+		}
+	if (!flag)
+		sup = size - 1;
+
+	struct Student* temp = (struct Student*)malloc(sizeof(struct Student) * (sup - inf));
+	int  j = 0;
+	for (int i = inf; i < poz; i++)
+	{
+		temp[j++] = ht[i];
+		ht[i].name = NULL;
+	}
+	for (int i = poz + 1; i <= sup; i++)
+	{
+		temp[j++] = ht[i];
+		ht[i].name = NULL;
+	}
+
+	for (int i = 0; i < (sup - inf); i++)
+		flag = insertStudent(ht, size, temp[i]);
+
+	free(temp);
+
+	return 1;
+}
+
 void main() {
 	FILE* f;
 	f = fopen("Students.txt", "r");
@@ -44,7 +118,7 @@ void main() {
 	struct Student s;
 
 	struct Student* HTable;
-	int size = ARRAY_SIZE;
+	unsigned int size = ARRAY_SIZE;
 
 	HTable = (struct Student*)malloc(size * sizeof(struct Student));
 	for (unsigned int i = 0; i < size; i++) {
@@ -61,7 +135,7 @@ void main() {
 		strcpy(s.name, token);
 
 		token = strtok(NULL, seps);
-		s.avg = atof(token);
+		s.avg = (float)atof(token);
 
 		printf("%d %s\n", s.id, s.name);
 
@@ -78,7 +152,7 @@ void main() {
 			}
 
 			insert = 1;
-			for (int i = 0; i < size && insert; i++) {
+			for (unsigned int i = 0; i < size && insert; i++) {
 				if (HTable[i].name)
 					insert = insertStudent(newHTable, newSize, HTable[i]);
 			}
@@ -98,4 +172,35 @@ void main() {
 	}
 
 	fclose(f);
+
+	int pos_student = searchStudent(HTable, size, "Gigel opescuP");
+	if (pos_student != -1)
+	{
+		// student identificat in tabela de dispersie
+		printf("\nStudent identificat pe pozitia %d: %d %s", pos_student, HTable[pos_student].id, HTable[pos_student].name);
+	}
+	else
+	{
+		printf("\nStudentul cautat nu exista in tabela de dispersie\n");
+	}
+
+	char sters = deleteStudent(HTable, size, "Ionescu Pavel");
+	if (sters)
+	{
+		// stergerea are loc
+		printf("\nContinut tabela de dispesie dupa stergere student:");
+		for (unsigned int i = 0; i < size; i++)
+		{
+			if (HTable[i].name)
+			{
+				printf("\nPosition %d: %s", i, HTable[i].name);
+			}
+		}
+	}
+	else
+	{
+		printf("\nStudentul cautat nu a fost identificat in tabela de dispersie.");
+	}
+
+	// dezalocarea structurii tabela de dispersie
 }
