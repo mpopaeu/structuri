1fc5ed64f16cdff180fda5ce2c74f42d1ae9a46a
Marius Popa <marius.popa@oracle.com>
2022-05-06 18:00:40 +0300

    add examples, week #8

--- 2021-2022/seminar/Grupa1047Sol/Grupa1047Proj/03_ListeDuble.c
+++ 2021-2022/seminar/Grupa1047Sol/Grupa1047Proj/03_ListeDuble.c
@@ -32,7 +32,11 @@ ListD insertListPos(ListD, NodeInfo*, unsigned int poz); // 1..n (poz = 0, 1 - i
 void printInfo(NodeInfo*);
 
 // splitare lista dubla la mijloc in 2 subliste
+ListD splitListD(ListD*);
 // dezalocare lista dubla
+ListD dezalocare_listaDubla(ListD);
+
+//liste simple/duble circulare
 
 void main()
 {
@@ -58,6 +62,7 @@ void main()
 			NodeInfo* info = createInfo(code, name, dept, salary);
 			list = insertListPos(list, info, poz);
 		}
+		fclose(pFile);
 
 		printf("\nLista Dubla dupa creare (inceput->sfarsit):\n");
 		Node* aux = list.first;
@@ -68,15 +73,140 @@ void main()
 		}
 
 		printf("\nLista Dubla dupa creare (sfarsit->inceput):\n");
-		aux = list.first;
+		aux = list.last;
 		while (aux)
 		{
 			printInfo(aux->info);
 			aux = aux->pPrev;
 		}
 
-		fclose(pFile);
+		ListD subList1, subList2;
+		subList1 = list;
+		
+		subList2 = splitListD(&subList1);
+
+		list.first = list.last = NULL; // lista initiala nu mai prezinta caracteristici de lista dubla; 
+									   // astfel, se trateaza list ca empty dupa operatia de split
+		
+		printf("\nSubLista Dubla #1 dupa split (inceput->sfarsit):\n");
+		aux = subList1.first;
+		while (aux)
+		{
+			printInfo(aux->info);
+			aux = aux->pNext;
+		}
+		printf("\nSubLista Dubla #1 dupa split (sfarsit->inceput):\n");
+		aux = subList1.last;
+		while (aux)
+		{
+			printInfo(aux->info);
+			aux = aux->pPrev;
+		}
+
+		printf("\nSubLista Dubla #2 dupa split (inceput->sfarsit):\n");
+		aux = subList2.first;
+		while (aux)
+		{
+			printInfo(aux->info);
+			aux = aux->pNext;
+		}
+		printf("\nSubLista Dubla #2 dupa split (sfarsit->inceput):\n");
+		aux = subList2.last;
+		while (aux)
+		{
+			printInfo(aux->info);
+			aux = aux->pPrev;
+		}
+
+		printf("\nLista Dubla initiala dupa split (inceput->sfarsit):\n");
+		aux = list.first;
+		while (aux)
+		{
+			printInfo(aux->info);
+			aux = aux->pNext;
+		}
+		printf("\nLista Dubla initiala dupa split (sfarsit->inceput):\n");
+		aux = list.last;
+		while (aux)
+		{
+			printInfo(aux->info);
+			aux = aux->pPrev;
+		}
+
+		subList1 = dezalocare_listaDubla(subList1);
+		printf("\nSubLista Dubla #1 dupa dezalocare (inceput->sfarsit):\n");
+		aux = subList1.first;
+		while (aux)
+		{
+			printInfo(aux->info);
+			aux = aux->pNext;
+		}
+		printf("\nSubLista Dubla #1 dupa dezalocare (sfarsit->inceput):\n");
+		aux = subList1.last;
+		while (aux)
+		{
+			printInfo(aux->info);
+			aux = aux->pPrev;
+		}
+
+		subList2 = dezalocare_listaDubla(subList2);
+		printf("\nSubLista Dubla #2 dupa dezalocare (inceput->sfarsit):\n");
+		aux = subList2.first;
+		while (aux)
+		{
+			printInfo(aux->info);
+			aux = aux->pNext;
+		}
+		printf("\nSubLista Dubla #2 dupa dezalocare (sfarsit->inceput):\n");
+		aux = subList2.last;
+		while (aux)
+		{
+			printInfo(aux->info);
+			aux = aux->pPrev;
+		}
 	}
+}
+
+ListD dezalocare_listaDubla(ListD list)
+{
+	while (list.first)
+	{
+		Node* tmp = list.first;
+		list.first = list.first->pNext;
+
+		free(tmp->info->dept); // dezalocare departament
+		free(tmp->info->name); // dezalocare nume angajat
+		free(tmp->info); // dezalocare structura angajat
+		free(tmp); // dezalocare nod lista dubla
+	}
+
+	list.last = NULL;
+
+	return list;
+}
+
+ListD splitListD(ListD* subList1)
+{
+	int counter = 0;
+	Node* aux = (*subList1).first;
+	while (aux) // determinare numar noduri din LD
+	{
+		counter++;
+		aux = aux->pNext;
+	}
+	aux = (*subList1).first;
+	int index = 1;
+	while (index++ < counter / 2) // pozitionare pe nodul din mijlocul LD
+		aux = aux->pNext;
+
+	ListD subList2;
+	subList2.first = aux->pNext; // primul nod din sublista 2
+	subList2.first->pPrev = NULL;
+	subList2.last = (*subList1).last; // ultimul nod din sublista 2
+
+	(*subList1).last = aux; // ultimul nod din subList1
+	(*subList1).last->pNext = NULL;
+	return subList2;
 }
 
 NodeInfo* createInfo(short code, char* name, char* dept, double salary)
