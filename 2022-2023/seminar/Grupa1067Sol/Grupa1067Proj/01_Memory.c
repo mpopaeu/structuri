accc080c31330e43600840404ab6a717193d4b11
Marius Popa <mepopa@MEPOPA-LAP.oradev.oraclecorp.com>
2023-03-08 17:51:53 +0200

    new examples - week #3

--- 2022-2023/seminar/Grupa1067Sol/Grupa1067Proj/01_Memory.c
+++ 2022-2023/seminar/Grupa1067Sol/Grupa1067Proj/01_Memory.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <malloc.h>
+
 #define VECTOR_SIZE 10
 
 int main()
@@ -16,15 +18,33 @@ int main()
 	pc = &v;
 
 	for (i = 0; i < VECTOR_SIZE / 2; i++)
-		v[i] = i + 1;
+		v[i] = i + 1;		// first half of the vector initialized by using variable v
 	for (; i < VECTOR_SIZE; i++)
-		pc[i] = i + 1;
+		pc[i] = i + 1;		// second half of the vector initialized by using variable pc
 
-	printf("c=%d,\" c=%c\n", c, c);
+	printf("c=%d,\" c=%c\n", c, c);	// print out same location named c in different formats (integer, ASCII)
 
 	short int* pi;
 	pi = &v;
 
+	printf("Array v parsed by pointer to char pc:\n");
+	for (i = 0; i < VECTOR_SIZE; i++)
+		printf("pc[%d]=%d\n", i + 1, pc[i]);
 
+	printf("Array v parsed by pointer to short int pi:\n");
+	for (i = 0; i < VECTOR_SIZE / 2; i++)
+		printf("pi[%d]=%d\n", i + 1, pi[i]);
+
+	pc = (char*)malloc(VECTOR_SIZE);
+	for (i = 0; i < VECTOR_SIZE; i++)
+		pc[i] = i + 1;
+
+	pi = (short int*)malloc(VECTOR_SIZE * sizeof(short int));
+	for (i = 0; i < VECTOR_SIZE; i++)
+		pi[i] = i * 10 + 1;
+
+
+	free(pc);
+	free(pi);
 	return 0;
 }
