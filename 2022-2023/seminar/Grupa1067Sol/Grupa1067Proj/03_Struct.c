f97d351fa0eb28166bdfbaec67cf901769ad5bd3
Marius Popa <marius.popa@oracle.com>
2023-03-22 16:39:20 +0200

    new examples - week #5

--- 2022-2023/seminar/Grupa1067Sol/Grupa1067Proj/03_Struct.c
+++ 2022-2023/seminar/Grupa1067Sol/Grupa1067Proj/03_Struct.c
@@ -1,3 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 256
+#define ACCOUNT_NUMBER 20
+
 struct BankAccount
 {
 	char* owner;
@@ -6,8 +14,39 @@ struct BankAccount
 	char currency[4];
 };
 
+// function to compute the number od currencies for each bank account owner
+// the banck account data is taken from an array (vba)
+
 int main()
 {
+	FILE* f;
+	f = fopen("Accounts.txt", "r");
 
+	unsigned char buffer[BUFFER_SIZE];
+	char sep_list[] = ",";
+
+	struct BankAccount vba[ACCOUNT_NUMBER];
+	unsigned int i = 0;
+
+	while (fgets(buffer, BUFFER_SIZE, f))
+	{
+		struct BankAccount ba;
+		char* token = strtok(buffer, sep_list); // owner's name
+		ba.owner = (char*)malloc(strlen(token) + 1);
+		strcpy(ba.owner, token); // there is no conversion
+
+		token = strtok(NULL, sep_list);
+		strcpy(ba.iban, token); // no conversion; text-to-text
+
+		token = strtok(NULL, sep_list);
+		ba.balance = atof(token); // conversion text-to-binary(as float)
+
+		token = strtok(NULL, sep_list);
+		strcpy(ba.currency, token);
+
+		vba[i++] = ba;
+	}
+
+	fclose(f);
 	return 0;
 }
