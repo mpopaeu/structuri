6d9c3caccfd84d8de8387132c4a8e8b406d16730
Marius Popa <marius.popa@oracle.com>
2023-03-29 16:41:09 +0300

    new examples - week #6

--- 2022-2023/seminar/Grupa1067Sol/Grupa1067Proj/03_Struct.c
+++ 2022-2023/seminar/Grupa1067Sol/Grupa1067Proj/03_Struct.c
@@ -14,8 +14,116 @@ struct BankAccount
 	char currency[4];
 };
 
-// function to compute the number od currencies for each bank account owner
+// function to compute the number of currencies for each bank account owner
 // the banck account data is taken from an array (vba)
+
+struct OwnerNode {
+	char* name;
+	struct CurrencyNode* currency_list;
+	struct OwnerNode* next;
+};
+
+struct CurrencyNode
+{
+	char currency[4];
+	struct CurrencyNode* next;
+};
+
+
+struct OwnerNode* create_owner_list(struct BankAccount v[], unsigned int v_size)
+{
+	struct OwnerNode* first = NULL;
+
+	for (unsigned int i = 0; i < v_size; i++)
+	{
+		struct OwnerNode* t = first;
+		unsigned char is_there = 0;
+		while (t && (is_there == 0))
+		{
+			if (strcmp(v[i].owner, t->name) == 0)
+			{
+				is_there = 1; // the owner is already placed in the list
+			}
+
+			t = t->next;
+		}
+
+		if (is_there == 0)
+		{
+			// the owner was not found out in the owners' list
+			// a new node is added to the owners' list
+			struct OwnerNode* new_node = (struct OwnerNode*)malloc(sizeof(struct OwnerNode));
+
+			new_node->name = (char*)malloc(strlen(v[i].owner) + 1);
+			strcpy(new_node->name, v[i].owner);
+
+			new_node->currency_list = NULL; // empty list of currencies to be attached to current owner
+			new_node->next = first; // next item of the new node is the existing starting itemt
+
+			first = new_node; // new 1st item of the list
+		}
+	}
+
+	return first;
+}
+
+struct CurrencyNode* create_currency_list(struct BankAccount v[], unsigned int v_size)
+{
+	struct CurrencyNode* first = NULL;
+
+	for (unsigned int i = 0; i < v_size; i++)
+	{
+		struct CurrencyNode* t = first;
+		unsigned char is_there = 0;
+		while (t && (is_there == 0))
+		{
+			if (strcmp(v[i].currency, t->currency) == 0)
+			{
+				is_there = 1; // the owner is already placed in the list
+			}
+
+			t = t->next;
+		}
+
+		if (is_there == 0)
+		{
+			// the owner was not found out in the owners' list
+			// a new node is added to the owners' list
+			struct CurrencyNode* new_node = (struct CurrencyNode*)malloc(sizeof(struct CurrencyNode));
+			strcpy(new_node->currency, v[i].currency);
+			new_node->next = first; // next item of the new node is the existing starting itemt
+
+			first = new_node; // new 1st item of the list
+		}
+	}
+
+	return first;
+}
+
+void attach_currencies_to_owners(struct OwnerNode* list, struct BankAccount v[], unsigned int size)
+{
+	for (unsigned int i = 0; i < size; i++)
+	{
+		unsigned char is_there = 0;
+		struct OwnerNode* t = list;
+		while (t && (is_there == 0))
+		{
+			if (strcmp(t->name, v[i].owner) == 0)
+			{
+				is_there = 1;
+				// t is the node in the list of owners to be check against the list of currencies
+				struct CurrencyNode* tcurrency = t->currency_list;
+				// parse the list of currencies
+				// check each node agaisnt the v[i]. currency
+				// add a new node in this list if the case
+			}
+
+			t = t->next;
+		}
+	}
+}
+
+// compute number of currencies per each owner
 
 int main()
 {
@@ -42,10 +150,36 @@ int main()
 		ba.balance = atof(token); // conversion text-to-binary(as float)
 
 		token = strtok(NULL, sep_list);
-		strcpy(ba.currency, token);
+		strcpy(ba.currency, token); // no conversion; text-to-text
 
 		vba[i++] = ba;
 	}
+
+	unsigned int array_size = i; // number of bank accounts read from the file Accounts.txt
+
+	struct OwnerNode* owner_list, *towner;
+	owner_list = create_owner_list(vba, array_size);
+	printf("List of bank acoount owners: \n");
+	towner = owner_list;
+	while (towner)
+	{
+		printf("Name: %s\n", towner->name);
+
+		towner = towner->next;
+	}
+
+	struct CurrencyNode* currency_list, *tcurrency;
+	currency_list = create_currency_list(vba, array_size);
+	printf("List of bank acoount currencies: \n");
+	tcurrency = currency_list;
+	while (tcurrency)
+	{
+		printf("Currency: %s\n", tcurrency->currency);
+
+		tcurrency = tcurrency->next;
+	}
+
+	// deallocate all structures created in heap mem
 
 	fclose(f);
 	return 0;
