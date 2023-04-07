361676cd4897ad0b86237b688f8021bbc9408f01
Marius Popa <marius.popa@oracle.com>
2023-04-07 13:23:09 +0300

    new examples - week #7

--- 2022-2023/curs/SeriaCSol/SeriaCProj/09_Tokenizare_altfel.c
+++ 2022-2023/curs/SeriaCSol/SeriaCProj/09_Tokenizare_altfel.c
@@ -12,11 +12,22 @@ int main() {
 	/* opening file for reading */
 	fp = fopen("Test.txt", "r");
 
+	printf("\n\nUtilizare fgets:\n"); // atentie la continut fisier pe linii
 	while (fgets(str, sizeof(str), fp) != NULL) {
 		/* writing content to stdout */
 		puts(str);
 		sscanf(str, "%d %d %[^\n]", &a, &b, c);
 		printf("a= %d, b= %d, c= %s\n", a, b, c);
+	}
+
+	
+	fseek(fp, 0, SEEK_SET);
+	printf("\n\nUtilizare fscanf:\n"); // atentie la continut fisier pe linii si ordine apel fscanf!
+	fscanf(fp, "%d %d %[^\n]", &a, &b, c);
+	while (!feof(fp))
+	{
+		printf("a= %d, b= %d, c= %s\n", a, b, c);
+		fscanf(fp, "%d %d %[^\n]", &a, &b, c);
 	}
 
 	fclose(fp);
