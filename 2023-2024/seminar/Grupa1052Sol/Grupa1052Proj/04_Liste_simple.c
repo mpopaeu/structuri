684826e6e7b90c9174d6810d2afd56ecf124e6b5
Marius Popa <marius.popa@oracle.com>
2024-03-25 19:30:02 +0200

    week #6

--- 2023-2024/seminar/Grupa1052Sol/Grupa1052Proj/04_Liste_simple.c
+++ 2023-2024/seminar/Grupa1052Sol/Grupa1052Proj/04_Liste_simple.c
@@ -39,6 +39,56 @@ NodLS* inserare_sfarsit(NodLS* prim, ContBancar cb)
 	return prim;
 }
 
+NodLS* interschimb_adiacente(NodLS* prim, char* cIBAN)
+{
+	if (prim && prim->next)
+	{
+		// exista cel putin 2 noduri in lista simpla
+		if (strcmp(prim->cont.iban, cIBAN) == 0)
+		{
+			// nodul cautat p este primul din lista simpla
+			NodLS* p = prim, * q = prim->next, * s = q->next;
+			p->next = s;
+			q->next = p;
+			prim = q;
+		}
+		else
+		{
+			// cazul general
+			// se cauta nodul p (incepand cu nodul #2) cu IBAN cIBAN
+			NodLS* p = prim->next;
+			NodLS* r = prim;
+			char vb = 0;
+			while ((p->next != NULL) && (vb == 0))
+			{
+				// parsez lista simpla pana la penultimul nod
+				if (strcmp(p->cont.iban, cIBAN) == 0)
+				{
+					NodLS* q = p->next, * s = q->next;
+					r->next = q;
+					p->next = s;
+					q->next = p;
+					vb = 1;
+				}
+				else
+				{
+					r = r->next; // r = p;
+					p = p->next;
+				}
+			}
+		}
+	}
+
+	return prim;
+}
+
+// interschimb noduri oarecare p = cIBAN1, q = cIBAN2
+NodLS* interschimb_oarecare(NodLS* prim, char* cIBAN1, char* cIBAN2)
+{
+}
+
+// sortare lista simpla cu interschimbare noduri la nivel de adrese de legatura
+
 int main()
 {
 
@@ -46,7 +96,7 @@ int main()
 
 	char buffer[256], separatori[] = ",\n";
 	unsigned char i_curent = 0;
-	struct Nod* primLS = NULL;
+	struct NodLS* primLS = NULL;
 
 	while (fgets(buffer, sizeof(buffer), f))
 	{
@@ -69,9 +119,47 @@ int main()
 	}
 
 	// parsare lista pentru validare
+	printf("Lista simpla dupa creare:\n");
+	NodLS* temp = primLS;
+	while (temp)
+	{
+		printf("%s %s\n", temp->cont.iban, temp->cont.titular);
+		temp = temp->next;
+	}
+
+	// interschimb noduri adiacente pe baza de iban
+	primLS = interschimb_adiacente(primLS, "RO13BRDE1234560987654321");
+
+	// parsare lista pentru validare
+	printf("Lista simpla dupa interschimb noduri adiacente:\n");
+	temp = primLS;
+	while (temp)
+	{
+		printf("%s %s\n", temp->cont.iban, temp->cont.titular);
+		temp = temp->next;
+	}
+
 
 
 	// dezalocare lista simpla
+	while (primLS)
+	{
+		temp = primLS; // nodul care se dezalocare
+		primLS = primLS->next; // noul inceput al listei simple
+
+		free(temp->cont.titular);	// dezalocare string titular cont bancar
+		free(temp);				// dezalocare nod
+	}
+
+	printf("Lista simpla dupa dezalocare:\n");
+	temp = primLS;
+	while (temp)
+	{
+		printf("%s %s\n", temp->cont.iban, temp->cont.titular);
+		temp = temp->next;
+	}
+
+
 	fclose(f);
 	return 0;
 }
--- 2023-2024/seminar/Grupa1064Sol/Grupa1064Proj/04_Liste_simple.c
+++ 2023-2024/seminar/Grupa1064Sol/Grupa1064Proj/04_Liste_simple.c
@@ -38,6 +38,128 @@ Nod* inserare_sfarsit(Nod* p, ContBancar cont)
 	return p;
 }
 
+// extensie pentru camp care poate avea valoare duplicata in lista simpla 
+// titular, sold, moneda
+Nod* interschimb_iban_adiacente(Nod* prim, char* cIBAN)
+{
+	if (prim == NULL || prim->next == NULL)
+		return prim;
+
+	if (strcmp(prim->cb.iban, cIBAN) == 0)
+	{
+		// se interschimba nodurile [1,2]
+		Nod* p = prim, * q = prim->next, * s = q->next;
+		p->next = s;
+		q->next = p;
+		prim = q;
+	}
+	else
+	{
+		// cazul general
+		Nod* p = prim->next, * r = prim;
+		while (p->next != NULL)
+		{
+			if (strcmp(p->cb.iban, cIBAN) == 0)
+			{
+				// iban exista in lista simpla
+				Nod* q = p->next, * s = q->next;
+				p->next = s;
+				q->next = p;
+				r->next = q;
+
+				return prim;
+			}
+			else
+			{
+				r = r->next; // r = p;
+				p = p->next;
+			}
+		}
+	}
+
+	return prim;
+}
+
+
+Nod* interschimb_iban_oarecare(Nod* prim, char* cIBAN1, char* cIBAN2)
+{
+	if (prim == NULL || prim->next == NULL)
+		return prim;
+
+	if (strcmp(prim->cb.iban, cIBAN1) == 0)
+	{
+		// cIBAN1 este pe nodul 1
+		Nod* p = prim, *q, *t;
+		if (strcmp(prim->next->cb.iban, cIBAN2) == 0)
+		{
+			// interschimb [1,2]
+			return interschimb_iban_adiacente(prim, cIBAN1);
+		}
+		else
+		{
+			// interschimb [1, i]
+			q = prim->next->next;
+			t = prim->next;
+			while (q)
+			{
+				if (strcmp(q->cb.iban, cIBAN2) == 0)
+				{
+					Nod* s = p->next, *w = q->next;
+					p->next = w;
+					q->next = s;
+					t->next = p;
+					return q;
+				}
+				else
+				{
+					q = q->next;
+					t = t->next;
+				}
+			}
+		}
+	}
+	else
+	{
+		if (strcmp(prim->cb.iban, cIBAN2) == 0)
+		{
+			Nod* p = prim;
+			// cIBAN2 este pe nodul 1
+			if (strcmp(prim->next->cb.iban, cIBAN1) == 0)
+			{
+				// interschimb [1,2]
+				return interschimb_iban_adiacente(prim, cIBAN2);
+			}
+			else
+			{
+				Nod* q, * t;
+				q = prim->next->next;
+				t = prim->next;
+				while (q)
+				{
+					if (strcmp(q->cb.iban, cIBAN1) == 0)
+					{
+						Nod* s = p->next, * w = q->next;
+						p->next = w;
+						q->next = s;
+						t->next = p;
+						return q;
+					}
+					else
+					{
+						q = q->next;
+						t = t->next;
+					}
+				}
+			}
+		}
+		else
+		{
+			// cazul [p, q]
+			// 1. q == p->next (adiacente in interior)
+			// 2. cazul general [cIBAN1,cIBAN2] sau [cIBAN2,cIBAN1]
+		}
+	}
+}
 int main()
 {
 	FILE* f = fopen("Conturi.txt", "r");
@@ -70,8 +192,33 @@ int main()
 		t = t->next;
 	}
 
-	// dezalocare lista simpla
 	// interschimb noduri lista simpla cu modificare adrese de legatura
+	prim = interschimb_iban_adiacente(prim, "RO04BRDE1234567890111222");
+	printf("Lista simpla dupa interschimb:\n");
+	t = prim;
+	while (t != NULL)
+	{
+		printf("%s %s\n", t->cb.iban, t->cb.titular);
+		t = t->next;
+	}
+
+	// dezalocare lista simpla
+	while (prim)
+	{
+		t = prim;
+		prim = prim->next;
+
+		free(t->cb.titular);
+		free(t);
+	}
+	printf("Lista simpla dupa dezalocare:\n");
+	t = prim;
+	while (t != NULL)
+	{
+		printf("%s %s\n", t->cb.iban, t->cb.titular);
+		t = t->next;
+	}
+	
 	fclose(f);
 	return 0;
 }
