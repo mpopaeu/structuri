d66fee78324955290650f9d22ea43f05a2a03f6a
Marius Popa <marius.popa@oracle.com>
2024-04-17 17:52:50 +0300

    week #9

--- 2023-2024/seminar/Grupa1073Sol/Grupa1073Proj/06_Hash_chaining.c
+++ 2023-2024/seminar/Grupa1073Sol/Grupa1073Proj/06_Hash_chaining.c
@@ -22,6 +22,61 @@ struct Node
 typedef struct BankAccount BankAccount;
 typedef struct Node Node;
 
+Node* insert_start(Node* list, BankAccount acc)
+{
+
+	Node* newNode = (Node*)malloc(sizeof(Node));
+	newNode->info = acc;
+	newNode->next = list;
+
+	return newNode;
+}
+
+unsigned char hash_function(char* key, unsigned char ht_size)
+{
+	// 1. convert the string into a number
+	unsigned int sum = 0;
+	for (unsigned char i = 0; i < (strlen(key) - 1); i++)
+		sum += key[i];
+	sum += key[strlen(key) - 1] * 13;
+
+	// 2. "translate" the above number into a valid offset for the hash table
+	return (sum % ht_size);
+}
+
+void insert_ba_data(Node** HT, unsigned char ht_size, BankAccount data)
+{
+	// 1. compute hash function to get a valid position/offset within HT
+	unsigned char offs = hash_function(data.iban, ht_size);
+
+	// 2. insert data into the simple list HT[offs]
+	HT[offs] = insert_start(HT[offs], data); // insert BankAccount data into simple list HT[offs]
+}
+
+BankAccount* search(Node** HT, unsigned char ht_size, char* key)
+{
+	// 1. compute hash function to get the offset of the simple list where data with key should be stored
+	unsigned char offs = hash_function(key, ht_size);
+
+	// 2. once having the simple list, look for the key for every items (nodes) in that list HT[offs]
+	Node* temp = HT[offs];
+	while (temp)
+	{
+		if (strcmp(key, temp->info.iban) == 0)
+		{
+			// the key matches the iben stored in the node temp
+			return &temp->info; // return the heap memory address of the BankAccount
+		}
+
+		temp = temp->next;
+	}
+
+	return NULL; // the BankAccount with key does not exist in the hash table
+}
+
+// create hash table with key owner_name
+// search for all Bank Account data for a certain owner provided as parameter to the search function
+
 int main()
 {
 	FILE* f = fopen("Accounts.txt", "r");
@@ -52,7 +107,39 @@ int main()
 		buffer[strlen(buffer) - 1] = 0;
 		strcpy(tAccount.currency, buffer);
 
+		// insert BankAccount data (tAccount) into hash table
+		insert_ba_data(HTable, HASH_TABLE_SIZE, tAccount);
 	}
+
+	// searching a BankAccount based on key
+	BankAccount* p_account_data = search(HTable, HASH_TABLE_SIZE, "Ionescu Georgica");
+	if (p_account_data != NULL)
+	{
+		// BankAccount data exists in hash table
+		printf("Bank Account data found out: %s %s\n", p_account_data->iban, p_account_data->owner_name);
+	}
+	else
+	{
+		printf("The searched Bank Account does not exist in the hash table!\n");
+	}
+
+	// deallocate hash table with chaining
+	// 1. deallocate all simple lists linked to the array HTable
+	for (unsigned int i = 0; i < HASH_TABLE_SIZE; i++)
+	{
+		while (HTable[i] != NULL)
+		{
+			Node* temp = HTable[i];
+			HTable[i] = HTable[i]->next;
+
+			free(temp->info.owner_name); // deallocate the owner's name of the BankAccount stored by temp
+			free(temp);					 // deallocate the node temp
+		}
+	}
+	// 2. deallocate the array as support data structure for the hash table
+	free(HTable);
+	HTable = NULL;
+
 
 	fclose(f);
 	return 0;
