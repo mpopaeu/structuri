935ce49e859d0db205e503f274c26211e3b3d2e1
Marius Popa <marius.popa@oracle.com>
2024-05-08 16:43:52 +0300

    week #10 part 2

--- 2023-2024/seminar/Grupa1060Sol/Grupa1060Proj/07_ABC.c
+++ 2023-2024/seminar/Grupa1060Sol/Grupa1060Proj/07_ABC.c
@@ -17,7 +17,7 @@ struct NodABC
 {
 	unsigned short int id_user;
 	User u;
-	struct NodABC *st, *dr;
+	struct NodABC* st, * dr;
 };
 
 typedef struct NodABC NodABC;
@@ -27,12 +27,105 @@ typedef struct NodABC NodABC;
 NodABC* inserare_user_ABC(NodABC* r, unsigned short int id, User usr)
 {
 	// TODO: implementare cu apel recursiv pentru cautare pozitie in arbore a noului nod
+	if (r != NULL)
+	{
+		//if(strcmp(nume,r->user.nume)>0)
+		if (r->id_user > id)
+		{
+			r->st = inserare_user_ABC(r->st, id, usr);
+		}
+		else if (r->id_user < id)
+		{
+			r->dr = inserare_user_ABC(r->dr, id, usr);
+		}
+	}
+	else
+	{
+		r = (NodABC*)malloc(sizeof(NodABC));
+		r->dr = NULL;
+		r->st = NULL;
+		r->u = usr;
+		r->id_user = id;
+
+	}
+
 
 	return r;
 }
 
+void afisareABCInordine(NodABC* rad) {
+	if (rad) {
+		//SRD
+		afisareABCInordine(rad->st);
+		printf("%d. Nume:%s, User:%s are varsta de %d, deschis la %s\n", rad->id_user, rad->u.nume_cont, rad->u.nume_user, rad->u.varsta, rad->u.deschis_la);
+		afisareABCInordine(rad->dr);
+	}
+}
+
+
+void afisareABCPreordine(NodABC* rad) {
+	if (rad) {
+		//RSD
+		printf("%d. Nume:%s, User:%s are varsta de %d, deschis la %s\n", rad->id_user, rad->u.nume_cont, rad->u.nume_user, rad->u.varsta, rad->u.deschis_la);
+		afisareABCPreordine(rad->st);
+		afisareABCPreordine(rad->dr);
+	}
+}
+
+void numaraFrunze(NodABC* rad, unsigned char* count)
+{
+	if (rad)
+	{
+		if (rad->dr == NULL && rad->st == NULL)
+		{
+			(*count)++;
+		}
+		numaraFrunze(rad->st, count);
+		numaraFrunze(rad->dr, count);
+	}
+}
+
+NodABC* dezalocareABC(NodABC* rad) {
+	if (rad) {
+		rad->st = dezalocareABC(rad->st);
+		rad->dr = dezalocareABC(rad->dr);
+		free(rad->u.nume_cont);
+		free(rad->u.nume_user);
+		free(rad);
+	}
+	return NULL;
+}
+
+User* cautaUserDupaId(NodABC* rad, unsigned short int idCautat) {
+	if (rad) {
+		if (rad->id_user < idCautat) {
+			return cautaUserDupaId(rad->dr, idCautat);
+		}
+		else if (rad->id_user > idCautat) {
+			return cautaUserDupaId(rad->st, idCautat);
+		}
+		else {
+			//idurile corespund- am gasit userul;
+			//return prin deep copy
+			User* user = (User*)malloc(sizeof(User));
+			strcpy(user->deschis_la, rad->u.deschis_la);
+			user->nume_cont = (char*)malloc(sizeof(char) * (strlen(rad->u.nume_cont) + 1));
+			strcpy(user->nume_cont, rad->u.nume_cont);
+			user->nume_user = (char*)malloc(sizeof(char) * (strlen(rad->u.nume_user) + 1));
+			strcpy(user->nume_user, rad->u.nume_user);
+			user->varsta = rad->u.varsta;
+			return user;
+		}
+	}
+	else {
+		return NULL;
+	}
+}
+
 int main()
 {
+
+
 	FILE* f = fopen("AccountsTree.txt", "r");
 	NodABC* rad = NULL;
 
@@ -68,10 +161,34 @@ int main()
 	fclose(f);
 
 	// parsare arbore binar de cautare (inordine)
+	afisareABCInordine(rad);
+	printf("\n");
+	afisareABCPreordine(rad);
 
 	// determinare numar de noduri frunza
+	unsigned char count = 0;
+	numaraFrunze(rad, &count);
+	printf("Numarul de frunze este: %d\n", count);
+
+	//cautare dupa id
+	User* user = cautaUserDupaId(rad, 399);
+	printf("Nume:%s, User:%s are varsta de %d, deschis la %s\n", user->nume_cont, user->nume_user, user->varsta, user->deschis_la);
 
 	// dezalocare arbore binar de cautare
+	rad = dezalocareABC(rad);
+	afisareABCInordine(rad);
+
+	//cod scris dupa seminar
+	//dezalocarea userului returnat prin DeepCopy
+	if (user) {
+		if (user->nume_cont) {
+			free(user->nume_cont);
+		}
+		if (user->nume_user) {
+			free(user->nume_user);
+		}
+		free(user);
+	}
 
 	return 0;
 }
