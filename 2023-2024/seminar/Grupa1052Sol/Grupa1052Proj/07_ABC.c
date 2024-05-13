a0dd97d1945d090a36df396b18a02d13371aa44a
Marius Popa <marius.popa@oracle.com>
2024-05-13 15:00:27 +0300

    week #12

--- 2023-2024/seminar/Grupa1052Sol/Grupa1052Proj/07_ABC.c
+++ 2023-2024/seminar/Grupa1052Sol/Grupa1052Proj/07_ABC.c
@@ -47,6 +47,69 @@ NodABC* inserare_cont_ABC(NodABC* r, ContBancar data, unsigned char* inserat)
 	return r;
 }
 
+void inordine_ABC(NodABC * r)
+{
+	if (r)
+	{
+		inordine_ABC(r->st);
+		printf("%d ", r->cont.cheie);
+		inordine_ABC(r->dr);
+	}
+}
+
+// I/ r nod din ABC (initial, r este nod redacina ABC
+// I/ key cheia contului bancare care se cauta in ABC
+// /O return adresa din NodABC a contului bancar cu key; altfel, NULL daca nu exista cont bancar cu key
+ContBancar* cauta_cont_bancar(NodABC* r, unsigned short int key)
+{
+	ContBancar* pdata = NULL;
+	if (r)
+	{
+		if (r->cont.cheie > key)
+		{
+			pdata = cauta_cont_bancar(r->st, key);
+		}
+		else
+		{
+			if (r->cont.cheie < key)
+			{
+				pdata = cauta_cont_bancar(r->dr, key);
+			}
+			else
+			{
+				pdata = &(r->cont);
+			}
+		}
+	}
+
+	return pdata;
+}
+
+
+NodABC* dezalocare_ABC(NodABC* r)
+{
+	if (r != NULL)
+	{
+		// parsare in post-ordine a ABC
+		r->st = dezalocare_ABC(r->st);
+		r->dr = dezalocare_ABC(r->dr);
+
+		// dezalocare nod curent
+		free(r->cont.titular);
+		free(r);
+		r = NULL;
+	}
+
+	return r;
+}
+
+
+// numarul de conturi bancare cu acelasi titular
+unsigned char nr_conturi_titular(NodABC* r, char* titular)
+{
+
+}
+
 int main()
 {
 
@@ -76,13 +139,36 @@ int main()
 
 		// inserare cont bancar in arbore binar de cautare
 		unsigned char inserat;
-		arbore = inserare_cont_ABC(arbore, tCont, &inserat);
+		arbore = inserare_cont_ABC(arbore, tCont, &inserat); // inserare nod in ABC
 		if (!inserat)
 		{
 			printf("Nodul %d nu a fost inserat!\n", tCont.cheie);
 			free(tCont.titular);
 		}
 	}
+
+	// traversare ABC dupa creare
+	printf("\nArbore traversat in inordine: ");
+	inordine_ABC(arbore);
+	printf("\n\n");
+
+	// cautare nod pe baza de cheie in ABC
+	ContBancar* p_cont = NULL;
+	p_cont = cauta_cont_bancar(arbore, 84);
+	if (p_cont != NULL)
+	{
+		printf("Cont bancar identificat: %d %s\n", p_cont->cheie, p_cont->iban);
+	}
+	else
+		printf("Contul bancar nu exista in ABC!\n");
+
+	// TODO: stergere nod in ABC
+
+	// dezalocare structura ABC
+	arbore = dezalocare_ABC(arbore);
+	printf("\nArborele dupa dezalocare: ");
+	inordine_ABC(arbore);
+	printf("\n\n");
 
 	fclose(f);
 	return 0;
