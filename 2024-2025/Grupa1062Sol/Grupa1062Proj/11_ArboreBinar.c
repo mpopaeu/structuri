0d6dd9eecbd3b9bd8eb3e11a9bb64942121ef9fb
Marius Popa <marius.popa@oracle.com>
2025-06-03 18:31:02 +0300

    1062 week #14

--- 2024-2025/Grupa1062Sol/Grupa1062Proj/11_ArboreBinar.c
+++ 2024-2025/Grupa1062Sol/Grupa1062Proj/11_ArboreBinar.c
@@ -138,6 +138,68 @@ NodArbore* stergere_frunze_nivel(NodArbore* r, unsigned char nivel)
 	return r;
 }
 
+// creare vector (dinamic) cu date card bancar (nr. card) din ABC avand sold peste medie
+
+void solduri_ABC(NodArbore* r, float *solduri, unsigned char *nr_noduri)
+{
+	if (r != NULL)
+	{
+		*solduri += r->cb.sold;
+		*nr_noduri += 1;
+		solduri_ABC(r->stanga, solduri, nr_noduri);
+		solduri_ABC(r->dreapta, solduri, nr_noduri);
+	}
+}
+
+void nr_carduri_ABC(NodArbore* r, float medie_solduri, unsigned char *nr_noduri)
+{
+	if (r != NULL)
+	{
+		if (r->cb.sold > medie_solduri)
+			*nr_noduri += 1;
+		nr_carduri_ABC(r->stanga, medie_solduri, nr_noduri);
+		nr_carduri_ABC(r->dreapta, medie_solduri, nr_noduri);
+	}
+}
+
+void scrie_vector(NodArbore* r, float medie_solduri, char** vector_solduri, unsigned char *i)
+{
+	if (r != NULL)
+	{
+		if (r->cb.sold > medie_solduri)
+		{
+			unsigned char j = *i;
+			vector_solduri[j] = r->cb.nr_card;
+			*i += 1;
+		}
+		scrie_vector(r->stanga, medie_solduri, vector_solduri, i);
+		scrie_vector(r->dreapta, medie_solduri, vector_solduri, i);
+	}
+}
+
+char** vector_peste_sold(NodArbore* r, unsigned char *size)
+{
+	// 1. determinare sold mediu carduri in ABC
+	float suma_solduri = 0; 
+	unsigned char nr_noduri = 0;
+	solduri_ABC(r, &suma_solduri, &nr_noduri);
+	float medie_solduri = suma_solduri / nr_noduri;
+
+	// 2. determinare nr. carduri cu sold peste medie
+	nr_noduri = 0; // reutilizare variabila nr_noduri
+	nr_carduri_ABC(r, medie_solduri, &nr_noduri);
+	*size = nr_noduri;
+
+	char** vector_solduri = malloc(nr_noduri * sizeof(char*));
+	unsigned char i = 0; // offset la care incepe scrierea in vector
+	// 3. populare vector cu date card bancar cu sold peste medie
+	scrie_vector(r, medie_solduri, vector_solduri, &i);
+
+	return vector_solduri;
+}
+
+
+
 int main()
 {
 	FILE* f = NULL;
@@ -192,6 +254,14 @@ int main()
 
 	printf("\n\nTraversare inordine arbore binar de cautare:\n");
 	traversare_inordine(root);
+
+	printf("Carduri cu sold peste medie:\n");
+	unsigned char size_vector = 0;
+	char** vector_sold_med = vector_peste_sold(root, &size_vector);
+	for (unsigned char i = 0; i < size_vector; i++)
+	{
+		printf("%s\n", vector_sold_med[i]);
+	}
 
 	printf("\n\nStergere frunze de pe nivel specificat:\n");
 	root = stergere_frunze_nivel(root, 4);
