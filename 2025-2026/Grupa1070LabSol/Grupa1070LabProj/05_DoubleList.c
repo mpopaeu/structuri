8f941633c7a72e49b65fe03fe154a7d51064b992
Marius Popa <marius.popa@oracle.com>
2026-03-27 09:53:30 +0200

    1070 week #6

--- 2025-2026/Grupa1070LabSol/Grupa1070LabProj/05_DoubleList.c
+++ 2025-2026/Grupa1070LabSol/Grupa1070LabProj/05_DoubleList.c
@@ -69,6 +69,70 @@ void parsingDList(DList list)
 	}
 }
 
+Employee deleteDNode(DList *list, unsigned char pos)
+{
+	Employee emp_result;
+	emp_result.name = NULL;
+
+	if (list->head != NULL)
+	{
+		unsigned char counter = 1;
+		NodeD* t = list->head;
+		while ((t != NULL) && (counter < pos))
+		{
+			t = t->next;
+			counter += 1;
+		}
+
+		if (t != NULL)
+		{
+			NodeD* p,* q;
+			p = t->prev; // the node before the one to be deleted
+			q = t->next; // the node after the one to be deleted
+
+			if (p != NULL && q != NULL)
+			{
+				// p and q are not endpoints of the double list
+				p->next = q;
+				q->prev = p;
+			}
+			else
+			{
+				if (p == NULL && q == NULL)
+				{
+					// there is one single node in the double list to be deleted
+					list->head = list->tail = NULL; // double list becomes empty
+				}
+				else
+				{
+					if (p == NULL)
+					{
+						// pos is 1; t is the head of the double list to be deleted
+						q->prev = p;
+						list->head = q; // the new head of the double list
+					}
+					else
+					{
+						// t is the last node in the double list
+						p->next = q;
+						list->tail = p; // the new tail of the double list
+					}
+				}
+			}
+
+			emp_result = t->emp; // save data to be returned to the caller
+			free(t); // deallocate the node t
+		}
+
+	}
+
+	return emp_result;
+
+}
+
+// function to remove all employess having the salary over a specified threshold
+// removed employees will be inserted into a different/separate double list
+
 int main()
 {
 	FILE* f;
@@ -110,7 +174,24 @@ int main()
 	parsingDList(double_list);
 
 	// delete a node on a certain position in double list
+	Employee result = deleteDNode(&double_list, 2);
+	if (result.name != NULL)
+		printf("\nDeleted employee: %s %s\n", result.CNP, result.name);
+	else
+		printf("\nNo employee deletion\n");
+	free(result.name); // deallocate the employee's name 
+	printf("\n\nDouble list after node deletion (both directions):\n");
+	parsingDList(double_list);
+
+
 	// deallocate the entire double list
+	while (double_list.head)
+	{
+		result = deleteDNode(&double_list, 1);
+		free(result.name); // deallocate the employee's name
+	}
+	printf("\n\nDouble list after list deallocation (both directions):\n");
+	parsingDList(double_list);
 
 	return 0;
 }
