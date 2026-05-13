60a41c36b913aef5f87de4bb028386762da605b7
Marius Popa <marius.popa@oracle.com>
2026-05-13 15:58:53 +0300

    1047 week #12

--- 2025-2026/Grupa1047LabSol/Grupa1047LabProj/09_ABC.c
+++ 2025-2026/Grupa1047LabSol/Grupa1047LabProj/09_ABC.c
@@ -75,6 +75,124 @@ void InordineABC(NodABC* nodCurent)
 	}
 }
 
+NodABC* dezalocareNoduriABC(NodABC* nodCurent)
+{
+	if (nodCurent != NULL)
+	{
+		nodCurent->stanga = dezalocareNoduriABC(nodCurent->stanga); // stergere noduri din subarborele stanga
+		nodCurent->dreapta = dezalocareNoduriABC(nodCurent->dreapta); // stergere noduri din subarborele dreapta
+
+		free(nodCurent->cl.denumire); // dezalocare extensie client in mem heap
+		free(nodCurent); // dezalocare nod
+
+		nodCurent = NULL;
+	}
+
+	return nodCurent;
+}
+
+Client* cautareClientCheie(NodABC* nodCurent, unsigned int cheie_client)
+{
+	if (nodCurent != NULL)
+	{
+		if (cheie_client < nodCurent->cl.id)
+		{
+			// se continua cautarea pe sub-arborele stanga
+			return cautareClientCheie(nodCurent->stanga, cheie_client);
+		}
+		else
+		{
+			if (cheie_client > nodCurent->cl.id)
+			{
+				// se continua cautarea pe sub-arborele dreapta
+				return cautareClientCheie(nodCurent->dreapta, cheie_client);
+			}
+			else
+			{
+				// noc curent contine cheie cautata
+				return &nodCurent->cl; // se returneaza adresa clientului din nod curent
+			}
+		}
+	}
+	
+	return NULL;
+}
+
+NodABC* stergereNodCheie(NodABC* nodCurent, unsigned int cheie_client, Client * client_extras)
+{
+	if (nodCurent != NULL)
+	{
+		if (cheie_client < nodCurent->cl.id)
+		{
+			// se continua cautarea pe sub-arborele stanga
+			nodCurent->stanga = stergereNodCheie(nodCurent->stanga, cheie_client, client_extras);
+		}
+		else
+		{
+			if (cheie_client > nodCurent->cl.id)
+			{
+				// se continua cautarea pe sub-arborele dreapta al nodului curent
+				nodCurent->dreapta = stergereNodCheie(nodCurent->dreapta, cheie_client, client_extras);
+			}
+			else
+			{
+				// nodCurent este nodul de sters
+				*client_extras = nodCurent->cl; // salvare date client in parametru de iesire din functie
+				if (nodCurent->stanga == NULL && nodCurent->dreapta == NULL)
+				{
+					// nodul de sters este nod frunza
+					free(nodCurent);
+					nodCurent = NULL; // permite salvarea in parinte a adresei de NULL pe directia lui nodCurent inainte de stergere
+				}
+				else
+				{
+					if (nodCurent->stanga != NULL && nodCurent->dreapta != NULL)
+					{
+						// nodul de sters are 2 descendenti
+						NodABC* desc_stanga = nodCurent->stanga;
+						NodABC* desc_dreapta = nodCurent->dreapta;
+
+						free(nodCurent);
+
+						NodABC* temp = desc_dreapta;
+						while (temp->stanga)
+							temp = temp->stanga;
+
+						temp->stanga = desc_stanga; // alipirea sub-arborelui stanga ca descendent stanga al nodului cu cheie minima din sub-arborele dreapta
+
+						nodCurent = desc_dreapta;
+					}
+					else
+					{
+						// nodul de sters are un singur descendent
+						NodABC* desc_unic = nodCurent->stanga;
+						if (nodCurent->dreapta != NULL)
+							desc_unic = nodCurent->dreapta;
+
+						free(nodCurent);
+						nodCurent = desc_unic;
+					}
+				}
+			}
+		}
+	}
+	else
+	{
+		// nodul cautat nu exista in ABC
+		client_extras->denumire = NULL;
+	}
+
+	return nodCurent;
+}
+
+// determinare nr de noduri frunza
+
+// determinare nr de noduri cu 2 descendenti
+
+// determinare noduri (salvare date) plasate pe un nivel specificat la ca parametru al functiei
+
+// determinare nr clienti cu valoarea comenzilor sub un prag specificat ca parametru
+
 int main()
 {
 	NodABC* root = NULL; // adresa nod radacina; pointer null inseamna structura ABC empty
@@ -99,7 +217,7 @@ int main()
 		c.tip = token[0]; // stocare valoare numerica (cod ASCII) in campul tip
 
 		token = strtok(NULL, sep); // identificare token urmator in buffer
-		strcpy(c.ultima_comanda, token); // copiee string in campul ultima_comanda
+		strcpy(c.ultima_comanda, token); // copiere string in campul ultima_comanda
 
 		token = strtok(NULL, sep); // identificare token urmator in buffer
 		c.valoare_totala_comenzi = (float)atof(token); // conversie ASCII-to-float si salvare rezultat in campul valoare_totala_comenzi
@@ -122,6 +240,35 @@ int main()
 	fclose(f);
 
 	printf("ABC dupa creare:\n");
+	InordineABC(root);
+
+	Client* pclient = cautareClientCheie(root, 81);
+	printf("\n///////////////// CAUTARE client dupa cheie ////////////////\n");
+	if (pclient != NULL)
+	{
+		printf("Client identificat in ABC: %d %s:", pclient->id, pclient->denumire);
+	}
+	else
+	{
+		printf("Clientul cauta nu exista in ABC");
+	}
+
+	root = stergereNodCheie(root, 12, &c);
+	printf("\n//////////// STERGERE CLIENT pe baza de cheie in ABC ////////////\n");
+	if (c.denumire != NULL)
+	{
+		printf("Client eliminat din ABC: %d %s\n", c.id, c.denumire);
+		free(c.denumire);
+	}
+	else
+	{
+		printf("Clientul nu a fost identificat in ABC.\n");
+	}
+	printf("\n//////////// ABC dupa stergere client pe baza de CHEIE ////////////\n");
+	InordineABC(root);
+
+	root = dezalocareNoduriABC(root);
+	printf("\n//////////// ABC dupa dezalocare ////////////\n");
 	InordineABC(root);
 
 	return 0;
